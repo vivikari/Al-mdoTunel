0da3d41bf652183aee1796eabaadb554d3846af9
Mellina <bizinoto.mellina@gmail.com>
2025-03-23 21:49:26 -0300

    Create server.js
    server.js vscode

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Servindo arquivos estáticos do front
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Rota principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(Servidor rodando em http://localhost:${PORT});
+});
